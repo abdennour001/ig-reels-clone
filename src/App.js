c3f2d8880d798e1d347c88e63884653e10f926b1
abdennour001 <amokranabdennour@gmail.com>
2020-08-15 23:06:50 +0100

    finish the footer 🦶

--- src/App.js
+++ src/App.js
@@ -29,24 +29,39 @@ function App() {
                 {/* Reels component */}
                 <VideoCard
                     onLeaveViewport={() => {
+                        isPlaying && dispatch({ type: "TOGGLE_PLAYING" });
+                    }}
+                    accountName="cleverqazi"
+                    accountImage="https://scontent-mrs2-2.cdninstagram.com/v/t51.2885-19/s320x320/22580466_127539667965809_8721119866930593792_n.jpg?_nc_ht=scontent-mrs2-2.cdninstagram.com&_nc_ohc=uPgHHjGHvY8AX_MLy4z&oh=86ec26b791d4368bb5fd47a6e43f981e&oe=5F619B31"
+                    song="Kash Man's Hot"
+                    likes={990}
+                    comments={309}
+                    videoSrc="https://scontent-mrs2-1.cdninstagram.com/v/t50.2886-16/117598442_205338364335864_545695080903278566_n.mp4?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=vjDPPklLnOYAX8-HsHQ&oe=5F3A4C4F&oh=c8e095a6ee74d4548b0b81348fd06ddc"
+                />
+                <VideoCard
+                    onLeaveViewport={() => {
                         if (isPlaying) {
                             dispatch({
                                 type: "TOGGLE_PLAYING"
                             });
-                        } 
+                        }
                     }}
+                    accountName="cleverqazi"
+                    accountImage="https://scontent-mrs2-2.cdninstagram.com/v/t51.2885-19/s320x320/22580466_127539667965809_8721119866930593792_n.jpg?_nc_ht=scontent-mrs2-2.cdninstagram.com&_nc_ohc=uPgHHjGHvY8AX_MLy4z&oh=86ec26b791d4368bb5fd47a6e43f981e&oe=5F619B31"
+                    song="Kash Man's Hot"
+                    likes={990}
+                    comments={309}
                     videoSrc="https://scontent-mrs2-1.cdninstagram.com/v/t50.2886-16/117950347_379983256320436_3527680957063597383_n.mp4?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=104&_nc_ohc=fV2FEHywlKYAX8b5HWb&oe=5F3A2B50&oh=5217378d279b076f78bcade922890050"
                 />
                 <VideoCard
                     onLeaveViewport={() => {
                         isPlaying && dispatch({ type: "TOGGLE_PLAYING" });
                     }}
-                    videoSrc="https://scontent-mrs2-1.cdninstagram.com/v/t50.2886-16/117598442_205338364335864_545695080903278566_n.mp4?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=vjDPPklLnOYAX8-HsHQ&oe=5F3A4C4F&oh=c8e095a6ee74d4548b0b81348fd06ddc"
-                />
-                <VideoCard
-                    onLeaveViewport={() => {
-                        isPlaying && dispatch({ type: "TOGGLE_PLAYING" });
-                    }}
+                    accountName="cleverqazi"
+                    accountImage="https://scontent-mrs2-2.cdninstagram.com/v/t51.2885-19/s320x320/22580466_127539667965809_8721119866930593792_n.jpg?_nc_ht=scontent-mrs2-2.cdninstagram.com&_nc_ohc=uPgHHjGHvY8AX_MLy4z&oh=86ec26b791d4368bb5fd47a6e43f981e&oe=5F619B31"
+                    song="Kash Man's Hot"
+                    likes={990}
+                    comments={309}
                     videoSrc="https://scontent-mrs2-1.cdninstagram.com/v/t50.2886-16/117598442_205338364335864_545695080903278566_n.mp4?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=vjDPPklLnOYAX8-HsHQ&oe=5F3A4C4F&oh=c8e095a6ee74d4548b0b81348fd06ddc"
                 />
             </main>
--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -2,10 +2,29 @@ import React, { useRef, useEffect } from "react";
 import "./VideoCard.css";
 import CameraAltOutlinedIcon from "@material-ui/icons/CameraAltOutlined";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import SendIcon from "@material-ui/icons/Send";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Ticker from "react-ticker";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+
 import handleViewport from "react-in-viewport";
+
 import { useStateValue } from "../../utils/StateProvider";
 
-function VideoCardSection({ videoSrc, inViewport, forwardedRef }) {
+function VideoCardSection({
+    videoSrc,
+    accountName,
+    accountImage,
+    song,
+    likes,
+    comments,
+    inViewport,
+    forwardedRef
+}) {
     const [{ isPlaying }, dispatch] = useStateValue();
 
     useEffect(() => {
@@ -42,7 +61,43 @@ function VideoCardSection({ videoSrc, inViewport, forwardedRef }) {
                 <h3>Reels</h3>
                 <CameraAltOutlinedIcon />
             </div>
-            <div className="video_card__footer"></div>
+            <div className="video_card__footer">
+                <div className="video_card__account">
+                    <Avatar src={accountImage} />
+                    <h4>{accountName}</h4>•
+                    <span className="video_card__follow">Follow</span>
+                </div>
+                <div className="video_card__song">
+                    <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <div className="video_card__ticker">
+                                    <MusicNoteIcon />
+                                    <h4>{song}</h4>
+                                </div>
+                            </>
+                        )}
+                    </Ticker>
+                </div>
+                <div className="video_card__actions">
+                    <div className="video_card__left">
+                        <FavoriteIcon />
+                        <ChatBubbleIcon />
+                        <SendIcon />
+                        <MoreHorizIcon />
+                    </div>
+                    <div className="video_card__right">
+                        <div className="video_card__likes">
+                            <FavoriteIcon fontSize="small" />
+                            <p>{likes}</p>
+                        </div>
+                        <div className="video_card__comments">
+                            <ChatBubbleIcon fontSize="small" />
+                            <p>{comments}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
