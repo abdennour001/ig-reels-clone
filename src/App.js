f0df9f6fd1117b8ed1412d9c8492a8ba461b178a
abdennour001 <amokranabdennour@gmail.com>
2020-08-15 16:37:39 +0100

    Init project 🚀

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import VideoCard from "./component/VideoCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            {/* Header section */}
+            <header className="app__header">
+                {/* Logos container  */}
+                <div className="app__logos">
+                    {/* Instagram logo */}
+                    <img src="" alt="IG" />
+                    {/* Reels logo */}
+                    <img src="" alt="Reels" />
+                </div>
+                {/* Title */}
+                <h2>Reels</h2>
+            </header>
+            <main className="app__main">
+                {/* Reels component */}
+                <VideoCard />
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function VideoCard() {
+    return <div className="video_card"></div>;
+}
+
+export default VideoCard;
