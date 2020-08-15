ff658776a49b623c866325799ccf798cf6777e60
abdennour001 <amokranabdennour@gmail.com>
2020-08-15 20:35:49 +0100

    finish play video 🔴

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import VideoCard from "./component/VideoCard";
+import VideoCard from "./components/VideoCard";
+import MovieIcon from "@material-ui/icons/Movie";
 
 function App() {
     return (
@@ -8,18 +9,36 @@ function App() {
             {/* Header section */}
             <header className="app__header">
                 {/* Logos container  */}
-                <div className="app__logos">
+                <div className="app__title">
                     {/* Instagram logo */}
-                    <img src="" alt="IG" />
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"
+                        alt="IG"
+                    />
                     {/* Reels logo */}
-                    <img src="" alt="Reels" />
+                    <MovieIcon className="app__reel" />
+                    {/* Title */}
+
+                    <h2>Reels</h2>
                 </div>
-                {/* Title */}
-                <h2>Reels</h2>
             </header>
             <main className="app__main">
                 {/* Reels component */}
-                <VideoCard />
+                <VideoCard
+                    onEnterViewport={() => console.log("1-enter")}
+                    onLeaveViewport={() => console.log("1-leave")}
+                    videoSrc="https://scontent-mrs2-1.cdninstagram.com/v/t50.2886-16/117950347_379983256320436_3527680957063597383_n.mp4?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=104&_nc_ohc=fV2FEHywlKYAX8b5HWb&oe=5F3A2B50&oh=5217378d279b076f78bcade922890050"
+                />
+                <VideoCard
+                    onEnterViewport={() => console.log("2-enter")}
+                    onLeaveViewport={() => console.log("2-leave")}
+                    videoSrc="https://scontent-mrs2-1.cdninstagram.com/v/t50.2886-16/117598442_205338364335864_545695080903278566_n.mp4?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=vjDPPklLnOYAX8-HsHQ&oe=5F3A4C4F&oh=c8e095a6ee74d4548b0b81348fd06ddc"
+                />
+                <VideoCard
+                    onEnterViewport={() => console.log("3-enter")}
+                    onLeaveViewport={() => console.log("3-leave")}
+                    videoSrc="https://scontent-mrs2-1.cdninstagram.com/v/t50.2886-16/117598442_205338364335864_545695080903278566_n.mp4?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=vjDPPklLnOYAX8-HsHQ&oe=5F3A4C4F&oh=c8e095a6ee74d4548b0b81348fd06ddc"
+                />
             </main>
         </div>
     );
--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import "./VideoCard.css";
+import CameraAltOutlinedIcon from "@material-ui/icons/CameraAltOutlined";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import handleViewport from "react-in-viewport";
 
-function VideoCard() {
-    return <div className="video_card"></div>;
+function VideoCardSection({ videoSrc, inViewport, forwardedRef }) {
+    useEffect(() => {
+        console.log(inViewport);
+    }, [inViewport]);
+
+    let videoRef = useRef(null);
+    let [isPlaying, setIsPlaying] = useState(true);
+
+    function toggleVideo() {
+        isPlaying ? videoRef.current.play() : videoRef.current.pause();
+        setIsPlaying(!isPlaying);
+    }
+
+    return (
+        <div className="video_card" ref={forwardedRef}>
+            <video
+                onClick={toggleVideo}
+                ref={videoRef}
+                className="video_card__reel"
+                src={videoSrc}
+                loop={true}
+            ></video>
+            <div className="video_card__header">
+                <ArrowBackIosIcon />
+                <h3>Reels</h3>
+                <CameraAltOutlinedIcon />
+            </div>
+            <div className="video_card__footer"></div>
+        </div>
+    );
 }
+const VideoCard = handleViewport(VideoCardSection, { rootMargin: "-200.0px" });
 
 export default VideoCard;
