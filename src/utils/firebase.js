f7ee02b10a81c2b3d80174cadaac3347a2e51c52
abdennour001 <amokranabdennour@gmail.com>
2020-08-16 09:45:48 +0100

    setting up firebase 🔥

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAjD1XPicu4YC5NBbTG9w1HDmsgQF3wA48",
+    authDomain: "ig-reels-clone-2c363.firebaseapp.com",
+    databaseURL: "https://ig-reels-clone-2c363.firebaseio.com",
+    projectId: "ig-reels-clone-2c363",
+    storageBucket: "ig-reels-clone-2c363.appspot.com",
+    messagingSenderId: "838270975098",
+    appId: "1:838270975098:web:71b250ad30722170768fc3"
+};
+
+const auth = firebase.filestore();
+
+export { auth };
